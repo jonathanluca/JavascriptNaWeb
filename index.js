ba0238ca883d84f9500dc3d649d47872560540f6
jonathan lucas <113566274+jonathanluca@users.noreply.github.com>
2023-02-09 19:29:27 -0300

    iniciando a junçao

---
+++ index.js
@@ -0,0 +1,28 @@
+// achando os html da minha tela
+
+let botaoMenos = document.getElementById('botao-menor')    // 
+let botaoMaior = document.getElementById('botao-maior')
+
+botaoMenos.addEventListener('click', subtrair)  // chamando uma funcao 
+botaoMaior.addEventListener('click', adicionar)
+
+botaoMaior.innerHTML= '+'       // indo no meu botao 
+botaoMenos.innerHTML= '-'
+numero.innerHTML = 0
+
+function adicionar(){
+    if(botaoMaior = numero.innerHTML){
+        console.log(`${numero.innerHTML++}`)
+    }     
+}
+
+function subtrair(){
+    if(botaoMenos = numero.innerHTML){
+        console.log(`${numero.innerHTML--}`);
+    }
+}
+
+
+
+
+
